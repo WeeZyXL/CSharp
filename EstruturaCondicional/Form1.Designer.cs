13c03ddd787afbfaa88a02795d4209199c6b074f
Wesley Ramos <weezy13br@gmail.com>
2025-03-06 19:42:01 -0300

    Add files via upload

---
+++ EstruturaCondicional/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace EstruturaCondicional
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Titulo = new System.Windows.Forms.Label();
+            this.lbNumero = new System.Windows.Forms.Label();
+            this.txtNumero = new System.Windows.Forms.TextBox();
+            this.btDescobrir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Titulo
+            // 
+            this.Titulo.AutoSize = true;
+            this.Titulo.BackColor = System.Drawing.Color.DarkKhaki;
+            this.Titulo.Font = new System.Drawing.Font("Comic Sans MS", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Titulo.Location = new System.Drawing.Point(114, 20);
+            this.Titulo.Name = "Titulo";
+            this.Titulo.Size = new System.Drawing.Size(175, 38);
+            this.Titulo.TabIndex = 0;
+            this.Titulo.Text = "Impar / Par";
+            // 
+            // lbNumero
+            // 
+            this.lbNumero.AutoSize = true;
+            this.lbNumero.BackColor = System.Drawing.Color.DarkKhaki;
+            this.lbNumero.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbNumero.Location = new System.Drawing.Point(61, 166);
+            this.lbNumero.Name = "lbNumero";
+            this.lbNumero.Size = new System.Drawing.Size(95, 29);
+            this.lbNumero.TabIndex = 1;
+            this.lbNumero.Text = "Número:";
+            // 
+            // txtNumero
+            // 
+            this.txtNumero.Location = new System.Drawing.Point(162, 175);
+            this.txtNumero.Name = "txtNumero";
+            this.txtNumero.Size = new System.Drawing.Size(127, 20);
+            this.txtNumero.TabIndex = 2;
+            // 
+            // btDescobrir
+            // 
+            this.btDescobrir.BackColor = System.Drawing.Color.Goldenrod;
+            this.btDescobrir.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btDescobrir.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btDescobrir.Location = new System.Drawing.Point(150, 265);
+            this.btDescobrir.Name = "btDescobrir";
+            this.btDescobrir.Size = new System.Drawing.Size(111, 41);
+            this.btDescobrir.TabIndex = 3;
+            this.btDescobrir.Text = "Descobrir";
+            this.btDescobrir.UseVisualStyleBackColor = false;
+            this.btDescobrir.Click += new System.EventHandler(this.btDescobrir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(407, 405);
+            this.Controls.Add(this.btDescobrir);
+            this.Controls.Add(this.txtNumero);
+            this.Controls.Add(this.lbNumero);
+            this.Controls.Add(this.Titulo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Titulo;
+        private System.Windows.Forms.Label lbNumero;
+        private System.Windows.Forms.TextBox txtNumero;
+        private System.Windows.Forms.Button btDescobrir;
+    }
+}
+
---
+++ EstruturaCondicional/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EstruturaCondicional
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btDescobrir_Click(object sender, EventArgs e)
+        {
+            int numero;
+            numero = int.Parse(txtNumero.Text);
+
+            if(numero % 2 == 0) {
+
+                MessageBox.Show("O número é Par");
+
+            } else
+            {
+                MessageBox.Show("O número é Impar");
+
+            }
+        }
+    }
+}
