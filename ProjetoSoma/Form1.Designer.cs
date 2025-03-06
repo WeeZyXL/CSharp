d9602cc20d18ecaf6f7e928146fad7ace54a842f
Wesley Ramos <weezy13br@gmail.com>
2025-03-06 19:56:07 -0300

    Add files via upload

---
+++ ProjetoSoma/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace ProjetoSoma
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Titulo = new System.Windows.Forms.Label();
+            this.Valor1 = new System.Windows.Forms.Label();
+            this.Valor2 = new System.Windows.Forms.Label();
+            this.Total = new System.Windows.Forms.Label();
+            this.txtvalor1 = new System.Windows.Forms.TextBox();
+            this.txtvalor2 = new System.Windows.Forms.TextBox();
+            this.txtTotal = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Titulo
+            // 
+            this.Titulo.AutoSize = true;
+            this.Titulo.Font = new System.Drawing.Font("Comic Sans MS", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Titulo.Location = new System.Drawing.Point(92, 39);
+            this.Titulo.Name = "Titulo";
+            this.Titulo.Size = new System.Drawing.Size(228, 45);
+            this.Titulo.TabIndex = 0;
+            this.Titulo.Text = "Projeto Soma";
+            // 
+            // Valor1
+            // 
+            this.Valor1.AutoSize = true;
+            this.Valor1.BackColor = System.Drawing.Color.Transparent;
+            this.Valor1.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Valor1.ForeColor = System.Drawing.Color.White;
+            this.Valor1.Location = new System.Drawing.Point(78, 173);
+            this.Valor1.Name = "Valor1";
+            this.Valor1.Size = new System.Drawing.Size(53, 18);
+            this.Valor1.TabIndex = 1;
+            this.Valor1.Text = "Valor A";
+            // 
+            // Valor2
+            // 
+            this.Valor2.AutoSize = true;
+            this.Valor2.BackColor = System.Drawing.Color.Transparent;
+            this.Valor2.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Valor2.ForeColor = System.Drawing.Color.White;
+            this.Valor2.Location = new System.Drawing.Point(78, 210);
+            this.Valor2.Name = "Valor2";
+            this.Valor2.Size = new System.Drawing.Size(52, 18);
+            this.Valor2.TabIndex = 2;
+            this.Valor2.Text = "Valor B";
+            // 
+            // Total
+            // 
+            this.Total.AutoSize = true;
+            this.Total.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Total.ForeColor = System.Drawing.Color.White;
+            this.Total.Location = new System.Drawing.Point(90, 252);
+            this.Total.Name = "Total";
+            this.Total.Size = new System.Drawing.Size(40, 18);
+            this.Total.TabIndex = 3;
+            this.Total.Text = "Total";
+            // 
+            // txtvalor1
+            // 
+            this.txtvalor1.Location = new System.Drawing.Point(150, 173);
+            this.txtvalor1.Name = "txtvalor1";
+            this.txtvalor1.Size = new System.Drawing.Size(100, 20);
+            this.txtvalor1.TabIndex = 4;
+            // 
+            // txtvalor2
+            // 
+            this.txtvalor2.Location = new System.Drawing.Point(150, 208);
+            this.txtvalor2.Name = "txtvalor2";
+            this.txtvalor2.Size = new System.Drawing.Size(100, 20);
+            this.txtvalor2.TabIndex = 5;
+            // 
+            // txtTotal
+            // 
+            this.txtTotal.Location = new System.Drawing.Point(151, 250);
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.Size = new System.Drawing.Size(99, 20);
+            this.txtTotal.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.button1.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(165, 287);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(85, 32);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Calcukar";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSlateBlue;
+            this.ClientSize = new System.Drawing.Size(399, 374);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtTotal);
+            this.Controls.Add(this.txtvalor2);
+            this.Controls.Add(this.txtvalor1);
+            this.Controls.Add(this.Total);
+            this.Controls.Add(this.Valor2);
+            this.Controls.Add(this.Valor1);
+            this.Controls.Add(this.Titulo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Titulo;
+        private System.Windows.Forms.Label Valor1;
+        private System.Windows.Forms.Label Valor2;
+        private System.Windows.Forms.Label Total;
+        private System.Windows.Forms.TextBox txtvalor1;
+        private System.Windows.Forms.TextBox txtvalor2;
+        private System.Windows.Forms.TextBox txtTotal;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
